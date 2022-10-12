e2d37e91495a96f85fd84857124d9b508a7aa836
Pedro <joao.pedro.xavibz@gmail.com>
2022-10-12 02:47:34 -0300

    Changed variable key value / removed database env vars

--- src/core/config.py
+++ src/core/config.py
@@ -5,11 +5,8 @@ load_dotenv()
 
 
 class Config:
-    HOST = os.getenv(key='LOCALHOST', default='localhost')
+    HOST = os.getenv(key='HOST', default='localhost')
     PORT = int(os.getenv(key='PORT', default=5000))
     DEBUG = bool(os.getenv(key='DEBUG', default=True))
     RELOAD = bool(os.getenv(key='RELOAD', default=True))
     LOG_LEVEL = os.getenv(key='LOG_LEVEL', default='info')
-
-    DB_URL = os.getenv(key='DB_URL', default='')
-    DB_NAME = os.getenv(key='DB_NAME', default='')
