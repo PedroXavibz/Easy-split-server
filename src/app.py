4a9fafb8c136593a44a4ba07198f98637bda4ec1
Pedro <joao.pedro.xavibz@gmail.com>
2022-10-12 02:58:23 -0300

    Included routes, added events to handle exceptions, removed events to connect/disconnect database on application startup/shutdown (I dont need a database)

--- src/app.py
+++ src/app.py
@@ -1,23 +1,19 @@
 from fastapi import FastAPI
-from db.base import DataBase
+from api.router import upload
+from utils.catch_exceptions import add_exception_handlers
 
 
 class App:
     def __init__(self):
         self.app = FastAPI()
+        self.include_routers()
 
     def start(self):
+        add_exception_handlers(app=self.app)
         return self.app
+
+    def include_routers(self):
+        self.app.include_router(prefix='/api/v1', router=upload.router)
 
 
 app = App().start()
-
-
-@app.on_event('startup')
-async def startup_db_client():
-    DataBase.connect()
-
-
-@app.on_event('shutdown')
-async def shutdown_db_client():
-    DataBase.close()
