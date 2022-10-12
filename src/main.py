730a4c1c3fa3347edee2cdfa067205c222b0fdc9
Pedro <joao.pedro.xavibz@gmail.com>
2022-10-12 02:59:14 -0300

    Formatted code

--- src/main.py
+++ src/main.py
@@ -2,5 +2,5 @@ import uvicorn
 from core.config import Config
 
 if __name__ == '__main__':
-    uvicorn.run(app='app:app', host=Config.HOST,
-                port=Config.PORT, log_level=Config.LOG_LEVEL, debug=Config.DEBUG, reload=Config.RELOAD)
+    uvicorn.run(app='app:app', host=Config.HOST, port=Config.PORT,
+                log_level=Config.LOG_LEVEL, debug=Config.DEBUG, reload=Config.RELOAD)
