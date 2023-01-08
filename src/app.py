4f1ed44936849413005a646a3b736cb2d03aaaab
Pedro <joao.pedro.xavibz@gmail.com>
2023-01-08 13:09:57 -0300

    Added Cors middleware

--- src/app.py
+++ src/app.py
@@ -1,16 +1,26 @@
 from fastapi import FastAPI
 from api.router import upload
 from utils.catch_exceptions import add_exception_handlers
+from fastapi.middleware.cors import CORSMiddleware
 
 
 class App:
     def __init__(self):
         self.app = FastAPI()
+        self.include_middlewares()
         self.include_routers()
 
     def start(self):
         add_exception_handlers(app=self.app)
         return self.app
+
+    def include_middlewares(self):
+        self.app.add_middleware(
+            CORSMiddleware,
+            allow_origins=['http://localhost:3000'],
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
 
     def include_routers(self):
         self.app.include_router(prefix='/api/v1', router=upload.router)
