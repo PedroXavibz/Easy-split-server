fe04dd8cf604c8c681e8243630b89d5d22a25ad2
Pedro <joao.pedro.xavibz@gmail.com>
2023-02-25 23:36:11 -0300

    update

--- src/core/config.py
+++ src/core/config.py
@@ -8,6 +8,6 @@ class Config:
     ORIGIN = os.getenv(key='ORIGIN', default='http://localhost:3000')
     HOST = os.getenv(key='HOST', default='localhost')
     PORT = int(os.getenv(key='PORT', default=5000))
-    DEBUG = bool(os.getenv(key='DEBUG', default=True))
-    RELOAD = bool(os.getenv(key='RELOAD', default=True))
+    DEBUG = os.getenv(key='DEBUG', default=True)
+    RELOAD = os.getenv(key='RELOAD', default=True)
     LOG_LEVEL = os.getenv(key='LOG_LEVEL', default='info')
